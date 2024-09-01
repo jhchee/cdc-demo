40173b8f6586a0e0bc5b2ab2b4d0b42ca226ae2b
jhchee <cheehong05090@gmail.com>
2024-09-01 12:26:20 +0100

    Refactor

--- bootstraps.sql
+++ bootstraps.sql
@@ -6,4 +6,10 @@ CREATE TABLE movies
 
 INSERT INTO movies (name)
 SELECT 'movie_' || s::text
-FROM generate_series(1, 100000) AS s;
+FROM generate_series(1, 1000) AS s;
+
+ALTER TABLE movies ADD COLUMN metadata JSONB;
+
+INSERT INTO movies (name, metadata)
+SELECT 'movie_' || s::text, '{"year": 2020, "rating": 5}'::jsonb
+FROM generate_series(1, 1000) AS s;
